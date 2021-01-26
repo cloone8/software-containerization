2f331b1390f6657768bd436e8bafeb37700be34e
Maurits Ouwerkerk <maurits.ouwerkerk@gmail.com>
2021-01-26 17:15:13 +0100

    readd

---
+++ ui/first-react/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Add quotes',
+        url: '/add',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Comment on quotes',
+        url: '/comment',
+        cName: 'nav-links'
+    },
+    // {
+    //     title: 'Products',
+    //     url: '#',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Contact Us',
+    //     url: '#',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Sign up',
+    //     url: '#',
+    //     cName: 'nav-links-mobile'
+    // }
+
+]
