842587692c4be2a6b91cfed7f2e510901cca0438
Maurits Ouwerkerk <maurits.ouwerkerk@gmail.com>
2021-02-01 16:33:42 +0100

    working ui

--- webapp/first-react/public/config.js
+++ webapp/first-react/public/config.js
@@ -1,1 +1,2 @@
-window.REST_API_URL = 'api.quotbook.io';
+window.REST_API_URL = 'https://api.quotbook.io/';
+// window.REST_API_URL = 'http://localhost:4321/';
--- webapp/first-react/src/components/quotes.js
+++ webapp/first-react/src/components/quotes.js
@@ -1,4 +1,18 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+
+
+const handleClick = (quote, quotes) => {
+    var toRemove = JSON.parse(JSON.stringify(quote));
+    const deleteQuote = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+    };
+    fetch(window.REST_API_URL + 'quote/' + toRemove.id, deleteQuote)
+        .then(response => response.json());
+
+    window.location.reload(); 
+}
 
 const Quotes = ({ quotes }) => {
     return (
@@ -7,8 +21,8 @@ const Quotes = ({ quotes }) => {
                 <div className="card">
                     <div className="card-body">
                         <h5 class="card-title">{"Author: " + quote.author}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{"Quote: " + quote.quote}</h6>
-                        {/* <p class="card-text">{quote}</p> */}
+                        <h6 className="card-subtitle mb-2 text-muted">{"Quote: " + quote.content}</h6>
+                        <Button variant="contained" color="primary" onClick={() => handleClick(quote, quotes)}>Delete Quote</Button>
                     </div>
                 </div>
             ))}
